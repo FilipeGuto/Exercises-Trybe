245cf5db7d3f9139bc4013020935ded21c5cd509
FilipeGuto <filipeguto95@gmail.com>
2021-05-20 11:37:36 -0300

    exercise 3

--- Bloco_4/exercises/dia_1_introducao_JS_logica_de_programacao/scripts.js
+++ Bloco_4/exercises/dia_1_introducao_JS_logica_de_programacao/scripts.js
@@ -1,6 +1,5 @@
 let a = 5;
 let b = 5;
-
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
@@ -9,7 +8,6 @@ console.log(a % b);
 
 let par = 28;
 let impar = 21;
-
 if (impar > par) {
   console.log(impar);
 }
@@ -20,7 +18,6 @@ else {
 let par = 8;
 let impar = 21;
 let centena = 105;
-
 if (impar > par && impar > centena) {
   console.log(impar);
 }
@@ -29,6 +26,17 @@ else if (par > impar && par > centena) {
 }
 else if (centena > impar && centena> par) {
   console.log(centena);
+}
+
+let valor = 0;
+if (valor > 0) {
+  console.log('positivo');
+}
+else if (valor < 0) {
+  console.log('negativo');
+}
+else {
+  console.log('zero');
 }
 
 
