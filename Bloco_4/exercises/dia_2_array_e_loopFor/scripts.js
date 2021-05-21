ebeaf75b33244be454aed3e7d40e8a53a6ed1c5b
FilipeGuto <filipeguto95@gmail.com>
2021-05-21 15:20:53 -0300

    exercise 6

--- Bloco_4/exercises/dia_2_array_e_loopFor/scripts.js
+++ Bloco_4/exercises/dia_2_array_e_loopFor/scripts.js
@@ -5,7 +5,6 @@ for (let index =0; index <numbers.length; index += 1) {
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0
-
 for (let index =0; index <numbers.length; index += 1) {
   soma = soma + numbers[index];
 };
@@ -18,7 +17,6 @@ for (let index =0; index <numbers.length; index += 1) {
   soma = soma + numbers[index];
   media = soma / numbers.length;
 };
-
 if (media > 20) {
   console.log('valor maior que 20');
 }
@@ -26,10 +24,20 @@ else {'valor menor que 20'};
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maior = 0
-
 for (let index =0; index <numbers.length; index += 1) {
   if (numbers[index] > maior) {
     maior = numbers[index];
   }
 }
 console.log(maior);
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = 0
+for (let index =0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !==0 ) {
+    console.log(impar = numbers[index]);
+  }
+  else if (numbers[index] % 2 !==0 === 0) {
+    console.log('nenhum valor ímpar encontrado');
+  }
+}
