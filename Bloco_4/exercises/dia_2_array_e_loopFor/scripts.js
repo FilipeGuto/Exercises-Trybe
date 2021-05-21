1c11cbd1bb7f11e15ba308a144b1d6f56ab4d511
FilipeGuto <filipeguto95@gmail.com>
2021-05-21 15:29:10 -0300

    exercise 7

--- Bloco_4/exercises/dia_2_array_e_loopFor/scripts.js
+++ Bloco_4/exercises/dia_2_array_e_loopFor/scripts.js
@@ -41,3 +41,12 @@ for (let index =0; index < numbers.length; index += 1) {
     console.log('nenhum valor ímpar encontrado');
   }
 }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorN = 100;
+for (let index =0; index <numbers.length; index += 1) {
+  if (numbers[index] < menorN) {
+    menorN = numbers[index];
+  }
+}
+console.log(menorN);
