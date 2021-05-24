82d21ba8714cc7472a0a459bfd93b5ec74cfd22e
FilipeGuto <filipeguto95@gmail.com>
2021-05-24 14:07:22 -0300

    Exercise 3

--- Bloco_4/exercises/dia_3_Lógica_de_Programação/Exercise_3.js
+++ Bloco_4/exercises/dia_3_Lógica_de_Programação/Exercise_3.js
@@ -3,10 +3,14 @@
 let quadrado = 5;
 let linha = ''
 let asteristico = '*'
+let space = ' '
 
 for (let index = 0; index < quadrado; index += 1) {
   for (let index2 = 0; index2 < index + 1; index2 += 1) {
-    linha = linha + asteristico;
+    linha =linha + asteristico;
+  }
+  for (let index3 = 0; index3 < quadrado - index -1; index3 += 1) {
+    linha = space + linha;
   }
   console.log(linha);
   linha = '';
