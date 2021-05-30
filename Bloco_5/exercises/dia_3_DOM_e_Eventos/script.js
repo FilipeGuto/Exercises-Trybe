31604c1bc4597c6f8659afc8901251bc257d9685
FilipeGuto <filipeguto95@gmail.com>
2021-05-30 11:49:43 -0300

    Exercise 1

--- Bloco_5/exercises/dia_3_DOM_e_Eventos/script.js
+++ Bloco_5/exercises/dia_3_DOM_e_Eventos/script.js
@@ -12,3 +12,40 @@ function createDaysOfTheWeek() {
 };
 
 createDaysOfTheWeek();
+
+//1 
+let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+let exe1 = document.querySelector('#days');
+
+function days() {
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let dias = dezDaysList[index];
+
+    let newDays = document.createElement('li');
+    newDays.innerText = dias;
+    newDays.className = 'day';
+    
+    if (dezDaysList[index] === 24) {
+    newDays.className = 'day holiday';
+    };
+    if (dezDaysList[index] === 25) {
+      newDays.className = 'day friday holiday';
+    };
+    if (dezDaysList[index] === 31) {
+      newDays.className = 'day holiday';
+    };
+    if (dezDaysList[index] === 4) {
+      newDays.className = 'day friday';
+    }
+    if (dezDaysList[index] ===11) {
+      newDays.className = 'day friday';
+    }
+    if (dezDaysList[index] ===18) {
+      newDays.className = 'day friday';
+    }
+
+    exe1.appendChild(newDays);
+  }
+}
+days();
