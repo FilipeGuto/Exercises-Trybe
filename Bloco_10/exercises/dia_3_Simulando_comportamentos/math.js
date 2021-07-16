b512fa0271b6cfefab698732381df4bbbbc6e1ad
FilipeGuto <filipeguto95@gmail.com>
2021-07-16 10:23:44 -0300

    Exercise 5

--- Bloco_10/exercises/dia_3_Simulando_comportamentos/math.js
+++ Bloco_10/exercises/dia_3_Simulando_comportamentos/math.js
@@ -1,10 +1,12 @@
-const sleep = (ms) => {
-  return new Promise(resolve => setTimeout(resolve, ms));
+const randomNumber = () => Math.floor(Math.random() * 101);
+module.exports = { randomNumber };
+
+const firstFunction = (str) => str.toUpperCase();
+const secondFunction = (str) =>  str.charAt(0);
+const thirdFunction = (str1, str2) => str1.concat(str2);
+
+module.exports = {
+  firstFunction,
+  secondFunction,
+  thirdFunction,
 };
-
-const somar = async (a, b) => { await sleep(10000); return a + b }; // Função de somar mais lenta do mundo
-const subtrair = (a, b) => a - b;
-const multiplicar = (a, b) => a * b;
-const dividir = (a, b) => a / b;
-
-module.exports = { somar, subtrair, multiplicar, dividir };
