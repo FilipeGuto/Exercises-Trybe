50a5b7c831f03d67c60c980f8298bd3b7dcc0913
FilipeGuto <filipeguto95@gmail.com>
2021-07-20 10:53:29 -0300

    Organizaçao Github

---
+++ Bloco_10/exercises/dia_3_Simulando_comportamentos/math.test.js
@@ -0,0 +1,102 @@
+const service = require('./math');
+
+//Exercicio 1 Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.
+test("testando se a função foi chamada, qual seu retorno e quantas vezes foi chamada", () => {
+  service.randomNumber = jest.fn().mockReturnValue(10);
+
+  expect(service.randomNumber()).toBe(10);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+});
+
+//Exercicio 2 nova implementação que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez.
+
+test("testando se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros", () => {
+  service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+
+  expect(service.randomNumber(10, 2)).toBe(5);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  expect(service.randomNumber).toHaveBeenCalledWith(10, 2);
+});
+
+
+//Exercicio 3 utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova que receba um parâmetro e retorne seu dobro.
+const service = require('./service');
+
+describe("testando implementações", () => {
+  test("mockando função para receber 3 parâmetros e retornar sua multiplicação", () => {
+    service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+    expect(service.randomNumber(2, 3, 4)).toBe(24);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(2, 3, 4);
+  });
+
+  test("mockando função que recebe um parâmetro e retorna seu dobro", () => {
+    service.randomNumber.mockReset();
+    expect(service.randomNumber).toHaveBeenCalledTimes(0);
+
+    service.randomNumber.mockImplementation(a => a * 2);
+
+    expect(service.randomNumber(5)).toBe(10);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(5);
+  });
+});
+
+//exercicio 4 Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a útlima letra de uma string. A terceira deve receber três strings e concatená-las.
+const service = require("./service");
+jest.mock("./service");
+
+describe("testando implementações", () => {
+  test("mockando função para receber um parâmetro e retornar em caixa baixa", () => {
+    service.firstFunction.mockImplementation(a => a.toLowerCase());
+
+    expect(service.firstFunction("UPPERCASE")).toBe("uppercase");
+    expect(service.firstFunction).toHaveBeenCalled();
+    expect(service.firstFunction).toHaveBeenCalledTimes(1);
+    expect(service.firstFunction).toHaveBeenCalledWith("UPPERCASE");
+  });
+
+  test("mockando função que recebe um parâmetro e retorna a última letra", () => {
+    service.secondFunction.mockImplementation(a => a.charAt(a.length - 1));
+
+    expect(service.secondFunction("letter")).toBe("r");
+    expect(service.secondFunction).toHaveBeenCalled();
+    expect(service.secondFunction).toHaveBeenCalledTimes(1);
+    expect(service.secondFunction).toHaveBeenCalledWith("letter");
+  });
+
+  test("mockando função que recebe 3 parâmetros e os concatena", () => {
+    service.thirdFunction.mockImplementation((a, b, c) => a.concat(b, c));
+
+    expect(service.thirdFunction("tr", "y", "be")).toBe("trybe");
+    expect(service.thirdFunction).toHaveBeenCalled();
+    expect(service.thirdFunction).toHaveBeenCalledTimes(1);
+    expect(service.thirdFunction).toHaveBeenCalledWith("tr", "y", "be");
+  });
+});
+
+//Exercicio 5 repita a implementação para a primeira função. Após repetir a implementação, restaure a implementação original e crie os testes necessários para validar.
+const service = require("./service");
+
+describe("testando implementações", () => {
+  test("mockando função para receber um parâmetro e retornar em caixa baixa", () => {
+    const first = jest
+      .spyOn(service, "firstFunction")
+      .mockImplementation(a => a.toLowerCase());
+
+    expect(first("UPPERCASE")).toBe("uppercase");
+    expect(first).toHaveBeenCalled();
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(first).toHaveBeenCalledWith("UPPERCASE");
+
+    service.firstFunction.mockRestore();
+
+    expect(service.firstFunction("lowercase")).toBe("LOWERCASE");
+  });
+});
+
